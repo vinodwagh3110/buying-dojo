fbd91af16a237e2c36b5079498a7691ba6a642d6
Wagh Vinod <waghv4339@gmail.com>
2026-02-15 22:27:44 +0530

    Fix router import and structure

--- buying-dojo/src/App.jsx
+++ buying-dojo/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/navbar";
 import CategoryPage from "./pages/CategoryPage";
